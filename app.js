66f49718fa2c6afa5ce38521e97ce803052632cc
Alex Algarate <alex.algarate@gmail.com>
2025-11-04 12:11:10 +0100

    feat: add some basic middlewares to app

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 import express from 'express';
-import { router as apiRouter } from './routes/apiRoutes.js';
+import { renderFile } from 'ejs';
+import morgan from 'morgan';
 
+import { router as apiRouter } from './routes/apiRoutes.js';
+import { connectMongoose } from './lib/connectMongoose.js';
 const app = express();
+
+// Mongoose connection
+const connection = await connectMongoose();
+console.log(`Connected to MongoDb: ${connection.name}`);
+
+// Middleware's
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
+app.use(morgan('dev'));
+
+// EJS
+app.set('view engine', 'ejs');
+app.engine('html', renderFile);
+app.set('views', './views');
 
 app.use('/api', apiRouter);
 
