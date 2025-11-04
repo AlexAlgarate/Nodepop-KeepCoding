967157ac516cdbb46fad7b9f5ba4c6373f01eeeb
Alex Algarate <alex.algarate@gmail.com>
2025-11-04 21:24:35 +0100

    feat: move formatTime to dateUtils

--- controllers/healthController.js
+++ controllers/healthController.js
@@ -1,16 +1,12 @@
 import { readFileSync } from 'fs';
 
+import { formatTime } from '../utils/dateutils.js';
+
 const packageJson = JSON.parse(
   readFileSync(new URL('../package.json', import.meta.url), 'utf8')
 );
 
 export function healthCall(req, res, next) {
-  const formatTime = (seconds) => {
-    const hours = Math.floor(seconds / 3600);
-    const minutes = Math.floor((seconds % 3600) / 60);
-    const secs = Math.floor(seconds % 60);
-    return `${hours}h ${minutes}m ${secs}s`;
-  };
   const seconds = process.uptime();
   res.status(200).json({
     status: 'Server Online',
---
+++ utils/dateutils.js
@@ -0,0 +1,6 @@
+export const formatTime = (seconds) => {
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  const secs = Math.floor(seconds % 60);
+  return `${hours}h ${minutes}m ${secs}s`;
+};
