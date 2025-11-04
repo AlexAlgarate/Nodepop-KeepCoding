430933adeb54d5ff7a2ac39be02a6079a598b81f
Alex Algarate <alex.algarate@gmail.com>
2025-11-04 12:46:30 +0100

    feat: create template Product Controller

---
+++ controllers/productController.js
@@ -0,0 +1,5 @@
+export const productController = {
+  getAllProducts: async (req, resizeBy, next) => {},
+  addProduct: async (req, resizeBy, next) => {},
+  removeProduct: async (req, resizeBy, next) => {},
+};
