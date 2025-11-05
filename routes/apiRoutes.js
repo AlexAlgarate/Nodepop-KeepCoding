2a87797a39b0fdf3c4049888dcddb41198765922
Alex Algarate <alex.algarate@gmail.com>
2025-11-05 20:47:25 +0100

    feat: add productController to api router

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,14 @@
 import express from 'express';
 import { healthCall } from '../controllers/healthController.js';
+import { productController } from '../controllers/productController.js';
 
 export const router = express.Router();
 
+// health endopint
 router.get(['/health', '/ping'], healthCall);
+
+// productController endpoints
+// router.get('/products', productController.getAllProducts);
+router.get('/products', productController.getAllProducts);
+router.post('/products', productController.addProduct);
+router.delete('/products/:id', productController.removeProduct);
