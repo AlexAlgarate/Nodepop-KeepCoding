01c4761879c47b8c355abb3d8318331687033174
Alex Algarate <alex.algarate@gmail.com>
2025-11-05 20:48:06 +0100

    feat: add getAll and add Product to productController

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,38 @@
+import { Product } from '../models/Product.js';
+
 export const productController = {
-  getAllProducts: async (req, resizeBy, next) => {},
-  addProduct: async (req, resizeBy, next) => {},
-  removeProduct: async (req, resizeBy, next) => {},
+  getAllProducts: async (req, res, next) => {
+    try {
+      const products = await Product.find().populate('owner');
+      res.status(200).json(products);
+    } catch (error) {
+      res.status(500).json({
+        message: 'Internal Server Error',
+        error: error.message,
+      });
+    }
+  },
+  addProduct: async (req, res, next) => {
+    const product = await Product({
+      name: req.body.name,
+      owner: req.body.owner,
+      price: req.body.price,
+      productTag: req.body.productTag,
+    });
+    try {
+      const savedProduct = await product.save();
+      res.status(201).json(savedProduct);
+    } catch (error) {
+      if (error.code && error.code === 11000) {
+        res.status(400).json({
+          message: 'Nombre duplicado',
+        });
+      } else {
+        res.status(500).json({
+          message: 'Internal Server Error',
+        });
+      }
+    }
+  },
+  removeProduct: async (req, res, next) => {},
 };
